de37787edee127961b92dd5d4765dc36532f9600
juancotera <juancoteracastro@gmail.com>
2024-09-05 21:21:23 -0500

    movile version

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ function enviarMensaje() {
     var enlace = "https://wa.me/" + numero + "?text=" + mensaje;
     window.open(enlace, "_blank");
 }
+
+// TESTIMONIOS
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.querySelector('.testimonios-container');
+    const testimonios = document.querySelectorAll('.testimonio');
+
+    // Clonar los testimonios para crear un efecto de scroll infinito
+    testimonios.forEach(testimonio => {
+        const clon = testimonio.cloneNode(true);
+        container.appendChild(clon);
+    });
+
+    // Ajustar la animación basada en el número de testimonios
+    const totalWidth = testimonios.length * (300 + 30); // ancho + gap
+    container.style.animationDuration = `${totalWidth / 50}s`; // Ajusta 50 para cambiar la velocidad
+});
