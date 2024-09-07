843d86e1fb490e1ed0440cf16d8ef46741f7e813
juancotera <juancoteracastro@gmail.com>
2024-09-06 21:20:18 -0500

    tablet-escritorio

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
+//ENVIAR MENSAJE POR WHATSAPP
+let enviar = document.getElementById("sent-wapp");
+enviar.addEventListener("click", enviarMensaje);
+
 function enviarMensaje() {
-    var nombre = document.getElementById("nombre").value;
-    var mensaje = "Hola soy " + encodeURIComponent(nombre) + " y quiero más información sobre las clases particulares https://www.educlassperu.com/";
-    var numero = "+51946768700"; // Reemplaza con el número de teléfono correcto
-    var enlace = "https://wa.me/" + numero + "?text=" + mensaje;
+    let nombre = document.getElementById("nombre").value;
+    let mensaje = "Hola soy " + encodeURIComponent(nombre) + " y quiero más información sobre las clases particulares https://www.educlassperu.com/";
+    let numero = "+51946768700"; // Reemplaza con el número de teléfono correcto
+    let enlace = "https://wa.me/" + numero + "?text=" + mensaje;
+    window.open(enlace, "_blank");
+}
+
+//ENVIAR MENSAJE PARA SEPARAR SU CLASE
+let separarClases = document.getElementById("separar-clases");
+separarClases.addEventListener("click", separarTuClase);
+
+function separarTuClase() {
+    let mensaje = "Hola Miss quiero separar mi clase. 🥳";
+    let numero = "+51946768700"; // Reemplaza con el número de teléfono correcto
+    let enlace = "https://wa.me/" + numero + "?text=" + mensaje;
+    window.open(enlace, "_blank");
+}
+
+//ENVIAR MENSAJE PARA SEPARAR SU CLASE
+let inscribirse = document.getElementById("inscribirse");
+inscribirse.addEventListener("click", inscribirCurso);
+
+function inscribirCurso() {
+    let mensaje = "Hola Maestra que capacitaciones tiene disponible ahora, quisiera información.";
+    let numero = "+51946768700"; // Reemplaza con el número de teléfono correcto
+    let enlace = "https://wa.me/" + numero + "?text=" + mensaje;
+    window.open(enlace, "_blank");
+}
+
+//CONTACTO
+const btnContac = document.getElementById("enviar-form");
+btnContac.addEventListener("click", enviarForm);
+
+function enviarForm() {
+    let name = document.getElementById("name-form").value;
+    let email = document.getElementById("email-form").value;
+    let text = document.getElementById("mensaje-form").value;
+    let concat = "Hola soy " + encodeURIComponent(name) +" mi correo es " + email + ", " + encodeURIComponent(text) + ".";
+    let numero = "+51946768700"; // Reemplaza con el número de teléfono correcto
+    let enlace = "https://wa.me/" + numero + "?text=" + concat;
     window.open(enlace, "_blank");
 }
 
@@ -19,5 +59,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Ajustar la animación basada en el número de testimonios
     const totalWidth = testimonios.length * (300 + 30); // ancho + gap
-    container.style.animationDuration = `${totalWidth / 50}s`; // Ajusta 50 para cambiar la velocidad
+    container.style.animationDuration = `${totalWidth / 20}s`; // Ajusta 50 para cambiar la velocidad
 });
